e58109cd2373e427bc9050cdbe29dfc7648a8fb5
kbarbounakis <k.barbounakis@gmail.com>
2015-01-20 10:14:20 +0200

    async ejs version 0.1.6

--- index.js
+++ index.js
@@ -1,50 +1,30 @@
-var common = require('common');
-var ejs = require('ejs');
 var fs = require('fs');
+var path = require('path');
+var ejs = require('ejs');
+var common = require('common');
 
-var adds = {};
-var creates = {};
+var noop = function() {};
+var fns = {file:noop};
 
-adds.raw = function(filename, callback) {
-	fs.readFile(filename, 'utf-8', callback);
+exports.fn = fns;
+
+var renderFile = function(file, options, callback) {
+    if (!callback) {
+        callback = options;
+        options = {};
+    }
+
+    common.step([
+        function(next) {
+            fs.readFile(file, 'utf-8', next);
+        },
+        function(template) {
+            render(template, options, callback);
+        }
+    ], callback);
 };
-creates.file = function(locals, options) {
-	return function(filename, locals, callback) {
-		var opt = common.join(options);
+exports.renderFile = renderFile;
 
-		if (!callback) {
-			callback = locals;
-			locals = options.locals;
-		} else {
-			locals = common.join(options.locals, locals);
-		}
-
-		opt.locals = locals;
-
-		exports.renderFile(filename, opt, callback);
-	};
-};
-
-exports.add = function(name, fn) {
-	adds[name] = fn;
-	return exports;	
-};
-exports.create = function(name, fn) {
-	creates[name] = fn;
-	return exports;	
-};
-exports.renderFile = function(filename, options, callback) {
-	if (!callback) {
-		callback = options;
-		options = {};
-	}
-	fs.readFile(filename, 'utf-8', common.fork(callback, function(src) {
-		exports.render(src, options, callback);
-	}));
-};
-
-//date:2014-11-12
-//change: ensure Object.getPrototypeOf() extension (browser support)
 if(!Object.getPrototypeOf) {
     if(({}).__proto__===Object.prototype&&([]).__proto__===Array.prototype) {
         Object.getPrototypeOf=function getPrototypeOf(object) {
@@ -58,17 +38,18 @@ if(!Object.getPrototypeOf) {
     }
 }
 
-exports.render = function(src, options, callback) {
-	if (!callback) {
-		callback = options;
-		options = {};
-	}
-	options.locals = options.locals || {};
-	//date:2014-11-12
-    //change: get async methods from options.locals object
+var render = function(src, options, callback) {
+    if (!callback) {
+        callback = options;
+        options = {};
+    }
+    options.locals = options.locals || {};
+
+    fns.file = function(filename, callback) {
+        renderFile(filename, options, callback);
+    };
     //get prototype of options.local
-    var fns = options.fns || { },
-        proto = Object.getPrototypeOf(options.locals);
+    var proto = Object.getPrototypeOf(options.locals);
     if (proto) {
         var keys = Object.keys(proto);
         for (var i = 0; i < keys.length; i++) {
@@ -77,60 +58,51 @@ exports.render = function(src, options, callback) {
                 fns[key] = proto[key];
         }
     }
+    var fns_local = {};
+    common.step([
+        function(next) {
+            var args = [];
+            Object.keys(fns).forEach(function(name) {
+                options.locals[name] = function() {
+                    args.push([name].concat(Array.prototype.slice.call(arguments)));
+                };
+            });
 
-	if (!fns) {
-		options.fns = fns = {};
+            var result = ejs.render(src, options);
+            if (!args.length) {
+                callback(null, result);
+                return;
+            }
 
-		for (var i in adds) {
-			fns[i] = adds[i];
-		}
-		for (var i in creates) {
-			fns[i] = creates[i](options.locals, options);
-		}
-	}
-	
-	common.step([
-		function(next) {
-			var args = [];
-						
-			Object.keys(fns).forEach(function(name) {
-				options.locals[name] = function() {
-					args.push([name].concat(Array.prototype.slice.call(arguments)));
-				};
-			});
-
-			var clone = common.join(options);
-
-			clone.locals = common.join(clone.locals);
-
-			var result = ejs.render(src, clone);
-			
-			if (!args.length) {
-				callback(null, result);
-				return;
-			}
-			
-			args.forEach(function(arg) {
-				var name = arg.shift();
-				
-				arg.push(next.parallel());
-                //date:2014-11-12
-                //change: set options.locals as local instance
-				fns[name].apply(options.locals, arg);
-			});
-		},
-		function(results, next) {
-			var i = 0;
-			
-			Object.keys(fns).forEach(function(name) {
-				options.locals[name] = function() {
-					return results[i++];
-				}
-			});
-			
-			src = ejs.render(src, options);
-		
-			callback(null, src);
-		}
-	], callback);
+            args.forEach(function(arg) {
+                var name = arg.shift();
+                arg.push(next.parallel());
+                fns[name].apply(options.locals, arg);
+            });
+        },
+        function(results, next) {
+            var i = 0;
+            Object.keys(fns).forEach(function(name) {
+                options.locals[name] = function() {
+                    return results[i++];
+                }
+            });
+            src = ejs.render(src, options);
+            callback(null,src);
+        }
+    ], callback);
 };
+exports.render = render;
+
+var add = function(name, fn) {
+    if (typeof name === 'object') {
+        for (var i in name) {
+            add(i, name[i]);
+        }
+        return exports;
+    }
+    fns[name] = fn;
+
+    return exports;
+};
+exports.add = add;
